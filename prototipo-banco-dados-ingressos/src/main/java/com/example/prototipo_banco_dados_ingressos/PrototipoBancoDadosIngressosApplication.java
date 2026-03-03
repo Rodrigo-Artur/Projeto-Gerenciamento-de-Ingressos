2e23af2ef7ebc50e85985c785a4e067460aaf7f3
Rodrigo Artur <rudrugoa@gmail.com>
2026-03-03 17:02:02 -0300

    Update 1.2

---
+++ prototipo-banco-dados-ingressos/src/main/java/com/example/prototipo_banco_dados_ingressos/PrototipoBancoDadosIngressosApplication.java
@@ -0,0 +1,13 @@
+package com.example.prototipo_banco_dados_ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipoBancoDadosIngressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipoBancoDadosIngressosApplication.class, args);
+	}
+
+}
---
+++ prototipo-banco-dados-ingressos/src/test/java/com/example/prototipo_banco_dados_ingressos/PrototipoBancoDadosIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prototipo_banco_dados_ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoBancoDadosIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
